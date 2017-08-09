6862c2831996c2d33238684829cc072604627591
Chris Bodenlos <hello_git@icloud.com>
2017-08-09 19:40:09 -0400

    Update email link insertion js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 
 window.onload = function() {
 
-    coded = "39ddc@I3Drz0cE39D.rc"
-    key = "YznZ4P8SqcdTQwm6r7thuH2XjCWeVIMgBOpbDN03ykJsFfR9GKaol1E5vUiLxA"
-    shift=coded.length
-    link=""
+    coded = "39ddc@I3Drz0cE39D.rc";
+    key = "YznZ4P8SqcdTQwm6r7thuH2XjCWeVIMgBOpbDN03ykJsFfR9GKaol1E5vUiLxA";
+    shift=coded.length;
+    link="";
     for (i=0; i<coded.length; i++) {
         if (key.indexOf(coded.charAt(i))==-1) {
-          ltr = coded.charAt(i)
-          link += (ltr)
+          ltr = coded.charAt(i);
+          link += (ltr);
         }
         else {     
-          ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length
-          link += (key.charAt(ltr))
+          ltr = (key.indexOf(coded.charAt(i))-shift+key.length) % key.length;
+          link += (key.charAt(ltr));
         }
     }
     
